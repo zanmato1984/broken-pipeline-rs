b522f3dc0ae90cde06fd0f353d4b8587b6e4b710
Rossi Sun <zanmato1984@gmail.com>
2026-03-24 14:38:31 -0700

    feat: surface channel input identities
    Signed-off-by: Rossi Sun <zanmato1984@gmail.com>

--- crates/broken-pipeline/src/pipeline.rs
+++ crates/broken-pipeline/src/pipeline.rs
@@ -4,6 +4,7 @@ use crate::operator::{SharedPipeOp, SharedSinkOp, SharedSourceOp};
 use crate::types::PipelineTypes;
 
 pub struct PipelineChannel<T: PipelineTypes> {
+    input_id: Option<usize>,
     source: SharedSourceOp<T>,
     pipes: Vec<SharedPipeOp<T>>,
 }
@@ -11,6 +12,7 @@ pub struct PipelineChannel<T: PipelineTypes> {
 impl<T: PipelineTypes> Clone for PipelineChannel<T> {
     fn clone(&self) -> Self {
         Self {
+            input_id: self.input_id,
             source: Arc::clone(&self.source),
             pipes: self.pipes.to_vec(),
         }
@@ -19,11 +21,31 @@ impl<T: PipelineTypes> Clone for PipelineChannel<T> {
 
 impl<T: PipelineTypes> PipelineChannel<T> {
     pub fn new(source: SharedSourceOp<T>, pipes: Vec<SharedPipeOp<T>>) -> Self {
-        Self { source, pipes }
+        Self {
+            input_id: None,
+            source,
+            pipes,
+        }
+    }
+
+    pub fn with_input_id(
+        input_id: usize,
+        source: SharedSourceOp<T>,
+        pipes: Vec<SharedPipeOp<T>>,
+    ) -> Self {
+        Self {
+            input_id: Some(input_id),
+            source,
+            pipes,
+        }
     }
 
     pub fn source(&self) -> &SharedSourceOp<T> {
         &self.source
+    }
+
+    pub fn input_id(&self) -> Option<usize> {
+        self.input_id
     }
 
     pub fn pipes(&self) -> &[SharedPipeOp<T>] {
